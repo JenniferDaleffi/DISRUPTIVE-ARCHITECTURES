91ff61d1908a8f7d86012265dc0b18ee2a5dde66
JenniferDalefi <jenniferdaleffii@gmail.com>
2025-02-19 20:31:33 -0300

    c++ code

---
+++ DISRUPTIVE ARCHITECTURES.cpp
@@ -0,0 +1,51 @@
+// R = 150 Ohms
+
+/* 
+Exemplo 1: Os 3 leds deverão piscar simultanemante em intervalos de 400ms
+
+Exemplo 2: Os leds verde e vermelho devereão  piscar alternadamente em intervalos de 400 ms
+
+Exemplo 3: 
+Crie um código para simular o semáforo.
+Verde --> 4 seg
+Amarelo --> 1 seg
+Vermelho --> 6 seg
+*/
+
+// Atribuir nome aos pinos e leds
+#define ledRED 2
+#define ledVEL 5
+#define ledGRE 9
+
+void setup() {
+  //EXEMPLO 2
+  pinMode(ledRED, OUTPUT);
+  pinMode(ledVEL, OUTPUT);
+  pinMode(ledGRE, OUTPUT);
+}
+
+void loop() {
+  ex1();
+  ex2();
+  ex3();
+
+}
+
+void ex1(){
+  digitalWrite(ledRED, HIGH);
+  digitalWrite(ledVEL, HIGH);
+  digitalWrite(ledGRE, HIGH);
+  delay(300);
+  digitalWrite(ledRED, LOW);
+  digitalWrite(ledVEL, LOW);
+  digitalWrite(ledGRE, LOW);
+  delay(300);
+}
+
+void ex2(){
+
+}
+
+void ex3(){
+
+}
